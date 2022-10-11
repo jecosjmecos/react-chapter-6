965387d61bc6c9fbcffda331120eaf8a745928b1
Eugeniy Stasuk <estasuk@markupus.tech>
2022-10-11 20:55:39 +0300

    131

---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,19 @@
+import './employees-list.css';
+import EmployeesListItem from '../employees-list-item/employees-list-item';
+
+const EmployeesList = ({data}) => {
+
+	const elements = data.map(item => {
+		return (
+			<EmployeesListItem key={item.id} name={item.name} salary={item.salary} increase={item.increase} />
+		);
+	});
+
+	return (
+		<ul className="app-list list-group">
+			{elements}
+		</ul>
+	);
+}
+
+export default EmployeesList;
